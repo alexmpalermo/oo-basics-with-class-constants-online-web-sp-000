e503f6802c0637967e6a137e60b781ae7010673d
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-28 18:14:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title
+  attr_reader :title, :genre
 
 GENRES = []
 
@@ -12,6 +12,9 @@ GENRES = []
     puts "Flipping the page...wow, you read fast!"
   end
 
-
+def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
 
 end
